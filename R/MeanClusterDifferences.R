37ccf183d11853dfbb43eab26adf83ddd60c0cde
Ewoud Ewing <45043947+EwoudEwing@users.noreply.github.com>
2022-08-31 15:02:10 +0200

    Create MeanClusterDifferences.R

---
+++ R/MeanClusterDifferences.R
@@ -0,0 +1,72 @@
+#' BreakUpCluster
+#'
+#' @import stats
+#' @import methods
+#' @import limma
+#'
+#' @importFrom stats dist hclust kmeans
+#'
+#' @param Object A PathwayObject
+#' @return PathwayObject
+#' @export
+
+MeanClusterDifferences <- function(Object)
+{
+  message("[=========================================================]")
+  message("[<<<<      MeanClusterDifferences START              >>>>>]")
+  message("-----------------------------------------------------------")
+  
+  RR.Data <- Object@Data.RR
+  Ncluster <- max(Object@Data[[1]]$cluster)
+  
+  Cluster.Combo <- t(combn(1:Ncluster, 2))
+  Cluster.Combo <- cbind(Cluster.Combo,
+                         matrix(NA, nrow = nrow(Cluster.Combo), ncol = 5))
+ colnames(Cluster.Combo) <- c("Cluster_A", "Cluster_B",
+                              "Cluster_A_Mean", "Cluster_B_Mean",
+                              "Diff_A-B", "Diffs_A", "Diffs_B")
+  
+  for(cl.i in 1:Ncluster)
+  {
+    idx_B <- Object@Data[[1]][Object@Data[[1]]$cluster == cl.i, "RR_name"]
+    
+    B.diffs.vc <- vector()
+    for(A.i in 1:length(idx_B))
+    {
+      for(B.i in 1:length(idx_B))
+      {
+        if(A.i == B.i){next()}
+        B.diffs.vc <- c(B.diffs.vc,
+                        abs(rowDiffs(as.matrix(RR.Data[c(idx_B[A.i],idx_B[B.i]),c(idx_B[A.i],idx_B[B.i])]))))
+      }
+    }
+    Cluster.Combo[Cluster.Combo[,"Cluster_A"] == cl.i,"Diffs_A"] <- mean(as.numeric(B.diffs.vc))
+    Cluster.Combo[Cluster.Combo[,"Cluster_B"] == cl.i,"Diffs_B"] <- mean(as.numeric(B.diffs.vc))
+  }
+  for(cl.i in 1:nrow(Cluster.Combo))
+  {
+    idx_A <- Object@Data[[1]][Object@Data[[1]]$cluster == Cluster.Combo[cl.i,1], "RR_name"]
+    idx_B <- Object@Data[[1]][Object@Data[[1]]$cluster == Cluster.Combo[cl.i,2], "RR_name"]
+    
+    Cluster.Combo[cl.i,"Cluster_A_Mean"] <- mean(as.numeric(unlist(RR.Data[idx_A,idx_A])))
+    Cluster.Combo[cl.i,"Cluster_B_Mean"] <- mean(as.numeric(unlist(RR.Data[idx_B,idx_B])))
+    
+    diffs.vc <- vector()
+    for(A.i in 1:length(idx_A))
+    {
+      for(B.i in 1:length(idx_B))
+      {
+        diffs.vc <- c(diffs.vc,
+                      abs(rowDiffs(as.matrix(RR.Data[c(idx_A[A.i],idx_B[B.i]),c(idx_A[A.i],idx_B[B.i])]))))
+      }
+    }
+    
+  
+    Cluster.Combo[cl.i,"Diff_A-B"] <- mean(as.numeric(diffs.vc))
+    
+  }
+ message("-----------------------------------------------------------")
+ message("[<<<<<      MeanClusterDifferences END              >>>>>>]")
+ message("[=========================================================]")
+ return(Cluster.Combo)
+}
