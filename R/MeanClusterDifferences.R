86cf5d7f101d76813d1237580718e196761acaec
Ewoud Ewing <45043947+EwoudEwing@users.noreply.github.com>
2022-08-31 16:35:41 +0200

    Update MeanClusterDifferences.R

--- R/MeanClusterDifferences.R
+++ R/MeanClusterDifferences.R
@@ -9,7 +9,6 @@
 #' @param Object A PathwayObject
 #' @return PathwayObject
 #' @export
-
 MeanClusterDifferences <- function(Object)
 {
   message("[=========================================================]")
@@ -21,28 +20,11 @@ MeanClusterDifferences <- function(Object)
   
   Cluster.Combo <- t(combn(1:Ncluster, 2))
   Cluster.Combo <- cbind(Cluster.Combo,
-                         matrix(NA, nrow = nrow(Cluster.Combo), ncol = 5))
+                         matrix(NA, nrow = nrow(Cluster.Combo), ncol = 3))
  colnames(Cluster.Combo) <- c("Cluster_A", "Cluster_B",
                               "Cluster_A_Mean", "Cluster_B_Mean",
-                              "Diff_A-B", "Diffs_A", "Diffs_B")
-  
-  for(cl.i in 1:Ncluster)
-  {
-    idx_B <- Object@Data[[1]][Object@Data[[1]]$cluster == cl.i, "RR_name"]
-    
-    B.diffs.vc <- vector()
-    for(A.i in 1:length(idx_B))
-    {
-      for(B.i in 1:length(idx_B))
-      {
-        if(A.i == B.i){next()}
-        B.diffs.vc <- c(B.diffs.vc,
-                        abs(rowDiffs(as.matrix(RR.Data[c(idx_B[A.i],idx_B[B.i]),c(idx_B[A.i],idx_B[B.i])]))))
-      }
-    }
-    Cluster.Combo[Cluster.Combo[,"Cluster_A"] == cl.i,"Diffs_A"] <- mean(as.numeric(B.diffs.vc))
-    Cluster.Combo[Cluster.Combo[,"Cluster_B"] == cl.i,"Diffs_B"] <- mean(as.numeric(B.diffs.vc))
-  }
+                              "Diff_A-B")
+
   for(cl.i in 1:nrow(Cluster.Combo))
   {
     idx_A <- Object@Data[[1]][Object@Data[[1]]$cluster == Cluster.Combo[cl.i,1], "RR_name"]
@@ -57,7 +39,7 @@ MeanClusterDifferences <- function(Object)
       for(B.i in 1:length(idx_B))
       {
         diffs.vc <- c(diffs.vc,
-                      abs(rowDiffs(as.matrix(RR.Data[c(idx_A[A.i],idx_B[B.i]),c(idx_A[A.i],idx_B[B.i])]))))
+                      RR.Data[c(idx_A[A.i]),c(idx_B[B.i])])
       }
     }
     
@@ -65,12 +47,11 @@ MeanClusterDifferences <- function(Object)
     Cluster.Combo[cl.i,"Diff_A-B"] <- mean(as.numeric(diffs.vc))
     
   }
-  
-  Cluster.Combo <- as.data.frame(Cluster.Combo)
- Cluster.Combo$Ratio_A_Diff <-  Cluster.Combo$`Diff_A-B` / Cluster.Combo$Diffs_A
- Cluster.Combo$Ratio_B_Diff <- Cluster.Combo$`Diff_A-B` / Cluster.Combo$Diffs_B
-  
-  
+ 
+ Cluster.Combo <- as.data.frame(Cluster.Combo)
+ Cluster.Combo$Ratio_A_Diff <-  Cluster.Combo$`Diff_A-B` / Cluster.Combo$Cluster_A_Mean
+ Cluster.Combo$Ratio_B_Diff <- Cluster.Combo$`Diff_A-B` / Cluster.Combo$Cluster_B_Mean
+ 
  message("-----------------------------------------------------------")
  message("[<<<<<      MeanClusterDifferences END              >>>>>>]")
  message("[=========================================================]")
