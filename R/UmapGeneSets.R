d1ae55703aa06f450bbc6f0ba1f1c63f55df3066
Ewoud Ewing <45043947+EwoudEwing@users.noreply.github.com>
2022-09-15 11:57:55 +0200

    Create UmapGeneSets.R

---
+++ R/UmapGeneSets.R
@@ -0,0 +1,50 @@
+#' UmapGeneSets
+#'
+#' @import stats
+#' @import methods
+#' @import limma
+#'
+#' @importFrom stats dist hclust kmeans umap
+#'
+#' @param Object A PathwayObject
+#' @param main Header for the plot
+#' @param labels Labels of GeneSets to be highlighted
+#' @export
+
+
+UmapGeneSets <- function(Object, main = "", labels = "")
+{
+ 
+  Object.umap <- umap(Object@Data.RR)
+  umap.labels <- (Object@Data[[1]]$cluster)
+  
+  colors=brewer.pal(n = max((Object@Data[[1]]$cluster)), name = "Set1")
+  
+  labels <- labels[labels %in% colnames(Object@Data.RR)]
+  
+  layout <- Object.umap$layout
+  
+  par(mar=c(5.2,4.1,4.1,2.1))
+  
+  xlim <- range(layout[,1])
+  ylim <- range(layout[,2])
+  
+  plot(xlim, ylim, type="n")
+  
+  points(layout[,1], layout[,2], col=colors[as.integer(umap.labels)],
+         cex=1, pch=16)
+  if(!labels[1] =="")
+  {
+   idx <-  which(colnames(Object@Data.RR) %in% labels)
+    
+   text(layout[idx,1], layout[idx,2], labels)
+  }
+  
+  labels.u <- unique(labels)
+  legend.pos <- "bottomright"
+  legend.text <- as.character(labels.u)
+  
+  legend(legend.pos, legend=legend.text, inset=0.03,
+         col=colors[as.integer(labels.u)],
+         pch=16)
+}
